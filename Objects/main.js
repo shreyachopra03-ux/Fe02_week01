a3ab4903a80b03c7f9ae3455e1172f9d76ed9bef
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-21 20:38:47 +0530

    Objects

--- Objects/main.js
+++ Objects/main.js
@@ -1,82 +1,88 @@
 // +++++++++++ object literals +++++++++++++
 
-
-// const blogs = [
-//     {title: 'why mac & cheese rules', likes: 30},
-//     {title: '10 things to make with marmite' , likes: 50}
-// ];
+const blogs = [
+    {
+        title: 'why mac & cheese rules',
+        likes: 30
+    },
+    {
+        title: '10 things to make with marmite' , 
+        likes: 50
+    }
+];
 // console.log(blogs);
 
 
-// let user = {
-//     name: "crystal",
-//     age: 30,
-//     email: 'crystal@.com',
-//     location: 'berlin',
-//     blogs: [
-//     {title: 'why mac & cheese rules', likes: 30},
-//     {title: '10 things to make with marmite' , likes: 50}
-//     ],
-//     login(){
-//         console.log('the user logged in');
-//     },
-//     logout(){
-//         console.log('the user logged out');
-//     },
-//     logBlogs(){
-//     //   console.log(this.blogs);
-//     console.log('the user has written the following blogs:')
-//     // yha forEach isliye lgaaya kyuki blogs ek array hai & arr ke hr element pr kaam krna hai toh forEach lgaana pdega ji
-//     this.blogs.forEach(blog => {
-//         console.log(blog.title + ':-' + blog.likes);
-//     })
-// }
-// };
-// user.logBlogs();
+let user = {
+    name: "crystal",
+    age: 30,
+    email: 'crystal@.com',
+    location: 'berlin',
+    blogs: [
+    {title: 'why mac & cheese rules', likes: 30},
+    {title: '10 things to make with marmite' , likes: 50}
+    ],
+    login(){
+        // console.log('the user logged in');
+    },
+    logout(){
+        // console.log('the user logged out');
+    },
+    logBlogs(){
+    console.log(this.blogs);
+    console.log('the user has written the following blogs:')
+    // yha forEach isliye lgaaya kyuki blogs ek array hai & arr ke hr element pr kaam krna hai toh forEach lgaana pdega 
+    this.blogs.forEach(blog => {
+        console.log(blog.title + ':-' + blog.likes);
+    })
+}
+};
+user.logBlogs();
 
-// console.log(user);
-// // dot notation
-// console.log(user.age);
+console.log(user);
 
-// // user.age = 35;
-// console.log(user.age);
+// dot notation
+console.log(user.age);
 
-// // square notation
-// console.log(user['location'])
-// console.log(user['email']);
-// user['name'] = 'chum-li';
-// console.log(user['name']);
+user.age = 35;
+console.log(user.age);
 
-// console.log(typeof user);
+// square notation
+console.log(user['location'])
+console.log(user['email']);
+user['name'] = 'chum-li';
+console.log(user['name']);
 
-// user.login();
-// user.logout();
-// user.logBlogs();
+console.log(typeof user);
+
+user.login();
+user.logout();
+user.logBlogs();
 
 // ++++++ method is always defined on an object, unlike a function ++++++++++
-// const hello = 'mario';
-// console.log(hello.toUpperCase());
+const hello = 'mario';
+console.log(hello.toUpperCase());
 
 
 // ++++++++++++ Math Object ++++++++++++++
 
-// console.log(Math);
-// console.log(Math.PI);
-// console.log(Math.E)
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E)
 
-// const area = 7.7;
+const area = 7.7;
 
-// console.log(Math.round(area));
-// console.log(Math.floor(area));
-// console.log(Math.ceil(area));
-// console.log(Math.trunc(area));  // trunc removes the decimal part
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));  // trunc removes the decimal part
 
 
-// // +++++++++++ random numbers ++++++++++++++
+// +++++++++++ random numbers ++++++++++++++
 
-// const random = Math.random();
-// console.log(random);               // random no. will always be a decimal no. b/w 0 & 1 
-// console.log(Math.round(random * 100));
+const random = Math.random();
+console.log(random);               // random no. will always be a decimal no. b/w 0 & 1 
+console.log(Math.round(random * 100));
 
 
 // ++++++++ primitive values +++++++++++
